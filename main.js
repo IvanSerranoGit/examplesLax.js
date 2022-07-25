8cef23be4e1e661d6b8c9a586f7f9b84df1f8b99
Iván Serrano <contacto.ivanserrano@gmail.com>
2022-07-25 14:34:46 -0500

    se sube un ejemplo del uso de lax.js

--- main.js
+++ main.js
@@ -1,25 +1,79 @@
+// window.onload = function () {
+//     window.addEventListener("scroll", (e) => {
+//         console.log(window.scrollY);
+//       });
+//     lax.init()
+
+//     // Add a driver that we use to control our animations
+//     lax.addDriver('scrollY', function () {
+//       return window.scrollY
+//     });
+
+//     // Add animation bindings to elements
+//     lax.addElements('.item', {
+//       scrollY: {
+//         opacity: [
+//             [0, 100],
+//             [1, 0]
+//           ],
+//           opacity: [
+//             [100, 200],
+//             [0, 1]
+//           ],
+//       }
+//     })
+//   }
+
+
 window.onload = function () {
-    window.addEventListener("scroll", (e) => {
-        console.log(window.scrollY);
-      });
     lax.init()
 
-    // Add a driver that we use to control our animations
     lax.addDriver('scrollY', function () {
       return window.scrollY
-    });
+    })
 
-    // Add animation bindings to elements
-    lax.addElements('.item', {
+    const container = document.querySelector('.container')
+    const count = 100
+
+    for (let i = 0; i < count; i++) {
+      const el = document.createElement('div')
+      el.className = "circle"
+      container.appendChild(el)
+    }
+
+    lax.addElements(".circle", {
       scrollY: {
+        translateX: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 'screenWidth/2', 'screenWidth'],
+          {
+            easing: 'easeInOutQuart',
+          }
+        ],
         opacity: [
-            [0, 100],
-            [1, 0]
-          ],
-          opacity: [
-            [100, 200],
-            [0, 1]
-          ],
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 1, 0],
+          {
+            easing: 'easeInOutCubic'
+          }
+        ],
+        "border-radius": [
+          ["elInY+200", "elCenterY", "elOutY-200"],
+          [0, 100, 0],
+          {
+            easing: 'easeInOutQuint',
+          }
+        ],
+        "box-shadow": [
+          ["elInY+200", "elCenterY", "elOutY-200"],
+          [50, 0, 50],
+          {
+            easing: 'easeInOutQuint',
+            cssFn: (val) => {
+              return `${val}px ${val}px ${val}px rgba(0,0,0,0.5)`
+            }
+          }
+        ],
       }
     })
   }
